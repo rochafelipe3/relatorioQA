74a00a0a6e480d7047aee788f1f04c08a903551d
Felipe Silva Rocha <144614457+rochafelipe3@users.noreply.github.com>
2024-06-25 11:43:49 -0300

    Add files via upload

--- artefatos/artefatos/VLPatVecTest.java
+++ artefatos/artefatos/VLPatVecTest.java
@@ -109,4 +109,188 @@ public class VLPatVecTest {
 		
 		assertThat(patvec).isNotNull();
 	}
+	
+	
+	//TESTES DE EDIÇÃO
+	
+	@Test
+	void editeVacinationValidDate() throws OHServiceException {
+		
+		var newVac = patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(newVac).isNotNull();
+	}
+	
+	@Test
+	public void editeVacinationInvalidDate() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void editeVacinationValidVaccineType() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void editeVacinationInvalidVaccineType() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+
+	@Test
+	public void editeVacinationValidVaccine() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNull();
+	}
+	
+	@Test
+	public void editeVacinationInvalidVaccine() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	//TESTES DE DELEÇÃO
+	
+	@Test
+	public void deleteVacinationValidVaccine() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNull();
+	}
+	
+	@Test
+	public void deleteVacinationInvalidVaccine() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	//TESTES DE CONSULTAR
+	
+	@Test
+	public void consultarVacinationInvalidDateFrom() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationInValidDateTo() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationInValidAgeFrom() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationInValidAgeTo() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationInValidSex() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationInValidVaccineType() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationInValidVaccine() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationInValidDateFrom() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationValidDateTo() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationValidAgeFrom() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationValidAgeTo() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationValidSex() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationValidVaccineType() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
+	
+	@Test
+	public void consultarVacinationValidVaccine() throws OHServiceException {
+		
+		patvacIoOperation.newPatientVaccine(patvec);
+		
+		assertThat(patvec).isNotNull();
+	}
 }
